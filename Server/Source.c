2a7201ff8b3dfe42db8dff39374eec35d30e6087
Настя Жулина <julina.nastia2017@yandex.ru>
2022-12-09 23:39:28 +0300

    server

--- Server/Source.c
+++ Server/Source.c
@@ -5,86 +5,70 @@
 
 int main()
 {
+    //сервер создает канал
     system("chcp 1251 > null");
-    LPSTR lpstrPipeName = L"\\\\.\\pipe\\MyPipe"; // имя канала (такое же, как и на севере)
-    HANDLE hNamePipe; 
-    BOOL flag_otvet = TRUE;
-    char message[SIZE_BUFFER]; 
-    DWORD actual_written; // сколько на самом деле было записано байт
-    LPWSTR buffer = &message; // строковая переменая, значение которой записывается в канал
+    HANDLE hNamePipe; //объявление дескриптора канала
+    LPSTR lpstPipeName = L"\\\\.\\pipe\\MyPipe"; // переменная, содержащая имя канала   
+    LPWSTR buffer = (CHAR*)calloc(SIZE_BUFFER, sizeof(CHAR)); // строковая перменная, в которую будут считаны даные
+    char message[SIZE_BUFFER]; // массив симвролов
+    BOOL Connected;
     DWORD actual_readen; // сколько на самом деле было прочитано байт
-    BOOL isSuccess;
-    BOOL SuccessRead;
+    //читать из канала
+    LPWSTR b = (CHAR*)calloc(SIZE_BUFFER, sizeof(CHAR));
+    LPWSTR zero = (CHAR*)calloc(SIZE_BUFFER, sizeof(CHAR));
     while (TRUE)
     {
-
-        hNamePipe = CreateFile( // открываем канал. по аналогии с открытием файла
-            lpstrPipeName,
-            GENERIC_READ | GENERIC_WRITE,
-            0,
-            NULL,
-            OPEN_EXISTING,
-            0,
-            NULL
+        HANDLE hNamePipe = CreateNamedPipe( // создание канала
+            lpstPipeName, // имя канала - путь
+            // и принимает и отправляет
+            PIPE_ACCESS_DUPLEX, // режм доступа к каналу (односторонний/двусторонний)
+            PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT, // режим работы канала: передавать сообщения| читать собщения | ждать
+            PIPE_UNLIMITED_INSTANCES, // количест во водящих соединений в каналу, в данном случае неогр.
+            SIZE_BUFFER, // объем буфера для чтения (байт)
+            SIZE_BUFFER, // объем буфера для записи (байт)
+            INFINITE, // максимальное время ожидания сообщения - ждать до конца
+            NULL // указатель на структуру безопасности
         );
-        DWORD dwMode = PIPE_READMODE_MESSAGE; //переключение в режим чтения и обратно
-        BOOL isSuccess = SetNamedPipeHandleState( //доступен сервер или нет
-            hNamePipe,
-            &dwMode,
-            NULL,
-            NULL
-        );
-        if (!isSuccess)
+        Connected = ConnectNamedPipe(hNamePipe, NULL); // установка с оединения клиента с каналом
+        if (Connected) // если клиент подключился
         {
-            printf("\nсервер не отвечает\n");
-            flag_otvet = TRUE;
+            printf("\nКлиент успешно подключен\n");
+
+            if (ReadFile(hNamePipe, buffer, SIZE_BUFFER, &actual_readen, NULL))
+            {
+                // обмен сообщениями
+                printf("\nКлиент пишет: ");
+                printf(buffer);
+
+                printf("\nСообщение: ");
+                gets(message);
+                buffer = &message;
+                WriteFile(hNamePipe, buffer, SIZE_BUFFER, &actual_readen, NULL);
+
+                // возведение в квадрат
+                //printf("\nКлиент пишет: \n");
+                //printf(buffer);// вывод на экран прочитанного сообщения
+                //printf("\n");
+                //float a = atof(buffer);// преобразует строку в значение типа double
+                //float aa = a * a; // в квадрат
+                //sprintf(message, "%g", aa); // вывод
+                //zero = L"0";
+                //sprintf(b, "%s", buffer);
+                //if (aa == 0 && strcmp(b, zero) != 0)
+                //{
+                //    sprintf(message, "Не корректно введены данные");
+                //}
+                //buffer = &message;
+                //WriteFile(hNamePipe, buffer, SIZE_BUFFER, &actual_readen, NULL);
+            }
+
+
         }
         else
         {
-            //чтение сообщений
-            LPSTR message = calloc(SIZE_BUFFER, sizeof(CHAR));
-            if (flag_otvet)
-            {
-                printf("Сообщение: ");
-                gets(message);
-                WriteFile(hNamePipe, message, SIZE_BUFFER, NULL, NULL);
-                flag_otvet = FALSE;
-            }
-            free(message);
-
-            message = calloc(SIZE_BUFFER, sizeof(CHAR));
-            if (ReadFile(hNamePipe, message, SIZE_BUFFER, NULL, NULL))
-            {
-                printf("Сервер: %s\n", message);
-                flag_otvet = TRUE;
-            }
-            free(message);
-
-
-            //if (flag_otvet)
-            //{
-            //    printf("\nВведите число для сервера:\n");
-            //    gets(message);
-            //    buffer = &message;
-            //    WriteFile(hNamePipe, buffer, SIZE_BUFFER, &actual_written, NULL); // запись в канал
-            //    flag_otvet = FALSE;
-
-            //}
-            ////читать с серваера
-            //SuccessRead = ReadFile(hNamePipe, buffer, SIZE_BUFFER, &actual_readen, NULL);
-            //if (SuccessRead)
-            //{
-            //    printf("\nОтвет:\n");
-            //    printf(buffer);
-            //    printf("\n");
-            //    flag_otvet = TRUE;
-            //}
+            printf("\nКлиент отключился от сервера\n");
         }
+        CloseHandle(hNamePipe); // закрываем канал
 
-
-        Sleep(100);
-        CloseHandle(hNamePipe);
     }
-
-
 }
